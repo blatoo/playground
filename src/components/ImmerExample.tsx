423907eaf019f4d52db58f1b97fe901ffea38c49
blatoo <connygy@googlemail.com>
2022-05-25 02:39:44 +0200

    installed immer and material ui

---
+++ src/components/ImmerExample.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+type Props = {}
+
+const studentStatus = {
+  name: "conny",
+  exam: {
+    math: 60,
+    english: 50,
+  },
+}
+
+export const ImmerExample = (props: Props) => {
+  return (
+    <>
+      <h1>ImmerExample</h1>
+      
+
+    </>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import React from "react"
+import { ImmerExample } from "../components/ImmerExample"
 
 type Props = {}
 
 export const Home = (props: Props) => {
-  return <div>Home</div>
+  return <div>
+    <ImmerExample></ImmerExample>
+  </div>
 }
