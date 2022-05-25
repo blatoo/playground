aefb4ef1ce5336cc157e760242d0d3b706507958
blatoo <connygy@googlemail.com>
2022-05-25 02:53:33 +0200

    js version for state change

--- src/components/ImmerExample.tsx
+++ src/components/ImmerExample.tsx
@@ -1,8 +1,9 @@
-import React from "react"
+import { Button } from "@mui/material"
+import React, { useState } from "react"
 
 type Props = {}
 
-const studentStatus = {
+const studentSummary = {
   name: "conny",
   exam: {
     math: 60,
@@ -11,11 +12,20 @@ const studentStatus = {
 }
 
 export const ImmerExample = (props: Props) => {
+  const [studentState, setStudentState] = useState(studentSummary)
+  const handleMathScore = () => {
+    setStudentState((sum) => {
+      return { ...sum, exam: { ...sum.exam, math: sum.exam.math + 10 } }
+    })
+  }
   return (
     <>
       <h1>ImmerExample</h1>
-      
 
+      <Button onClick={handleMathScore}>Math Pass</Button>
+      <p>name: {studentState.name}</p>
+      <p>math: {studentState.exam.math}</p>
+      <p>english: {studentState.exam.english}</p>
     </>
   )
 }
